fa8ec2c1672a753acea4f527b2475bc2762e881b
Dgibson89 <donovan.gibson89@gmail.com>
2023-10-04 09:11:30 -0500

    Initial commit of random quotes Simple fetch request pulling a random quote with its author to display. Theme button added because dark mode is best but people deserve options, even its wrong.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { FaQuoteLeft } from "react-icons/fa";
 
 function App() {
+  const [quote, setQuote] = useState({ content: "", author: "" });
+
+  const [darkMode, setDarkMode] = useState(true);
+
+  useEffect(() => {
+    fetch("https://api.quotable.io/random")
+      .then((response) => response.json())
+      .then((data) => {
+        setQuote({
+          content: data.content,
+          author: data.author,
+        });
+      })
+      .catch((error) => console.error("Error fetching quote:", error));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div
+      className={`container ${
+        darkMode ? "text-white bg-dark" : "text-dark bg-light"
+      }`}
+    >
+      <div className="d-flex flex-column align-items-center justify-content-center vh-100">
+        <FaQuoteLeft size={50} />
+        <p className="mt-3">{quote.content}</p>
+        <p className="font-italic">- {quote.author}</p>{" "}
+        {/* Displaying the author here */}
+        <button
+          onClick={() => setDarkMode(!darkMode)}
+          className={`btn mt-3 ${darkMode ? "btn-light" : "btn-dark"}`}
         >
-          Learn React
-        </a>
-      </header>
+          Toggle Theme
+        </button>
+      </div>
     </div>
   );
 }
